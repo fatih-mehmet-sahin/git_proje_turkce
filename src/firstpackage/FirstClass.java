b4293204b7ab3e50fab440b9d76f3d1b92d170cb
Fatih <fatihshn35@gmail.com>
2020-06-21 15:07:55 +0300

    firs commit from local repo

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,8 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hebele Hubele");
+		System.out.println("first commit from local repository");
+		
 
 	}
 
